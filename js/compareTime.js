c86afdd378a93b1179cf6a0f59c50a2e67266560
Calum Smith <c.smith16@gmail.com>
2018-11-03 11:59:53 +0000

    Post-midday updates.

--- js/compareTime.js
+++ js/compareTime.js
@@ -1,32 +1,45 @@
 /*
 
-• define the banner to show/hide
+// define the banner to show/hide
 var banner = document.getElementsByClassName('banner')[0];
 
-• define variable for current time
-var getTime = ..........
+// define variable for current time and convert to timestamp
+var t = new Date();
+var currentTime = t.getTime();
 
-• define variable for time (in standard unit) to show banner
-var startTime = ..........
+function toTimestamp(date) {
+    return Date.UTC(
+        date.getFullYear(),
+        date.getMonth(),
+        date.getDate(),
+        date.getHours(),
+        date.getMinutes(),
+        date.getSeconds(),
+        date.getMilliseconds()
+    );
+}
+toTimestamp(t);
 
-• define variable for time (in standard unit) to re-hide banner [after event] (optional)
-var endTime = ..........
+// define variable for time to show banner and convert to timestamp
+var s = new Date("2018-11-03T12:00:00Z");
+var startTime = s.getTime();
+// should be 1541246400000
 
-• convert to same standard unit as show/hide times
-var currentTime = getTime.SOMEFUNCTION()
+// define variable for time to re-hide banner [after event] and convert to timestamp (optional)
+var e = new Date("2018-11-03T23:59:59Z");
+var endTime = e.getTime();
+// should be 1541289599000
 
-
-• if current time > start time AND <= end time then show the banner
+// if current time > start time AND <= end time then show the banner
 if (currentTime > startTime	&& currentTime <= endTime) {..........}
 
-• else if current time > end time then hide the banner
+// else if current time > end time then hide the banner
 else if (currentTime > endTime) {..........}
 
-
-• define function
+// define function
 function compareTime() {..........}
 
-• run function on page load
+// run function on page load
 window.onload = function() {
   compareTime();
 };
